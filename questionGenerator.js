e619248b0be5c30b24b880ebe9b7e803bc51115c
bulkacity <112503952+bulkacity@users.noreply.github.com>
2022-10-24 22:59:50 -0400

    Adding more questions

--- questionGenerator.js
+++ questionGenerator.js
@@ -1,9 +1,9 @@
 // The following will generate the questions as objects within an array
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "Following are the JavaScript Data types:",
+      choices: ["Numbers", "String", "Boolean", "Object","Undefined","All of the above"],
+      answer: "All of the above"
     },
     {
       title: "The condition in an if / else statement is enclosed within ____.",
@@ -11,19 +11,33 @@ var questions = [
       answer: "parentheses"
     },
     {
-      title: "Which of these is not used to loop?",
-      choices: ["for", "while", "foreach", "sequence"],
-      answer: "sequence"
+      title: "True or False, isNan function returns true if the argument is not a number; otherwise, it is false.",
+      choices: ["True", "False", "N/A"],
+      answer: "True"
     },
     {
-      title: "Which of these is not a way to save a variable",
-      choices: ["vet", "var", "let", "const"],
-      answer: "vet"
+      title: "True or False, 'This' keyword refers to the object from where it was called.",
+      choices: ["True", "False","N/A"],
+      answer: "True"
     },
     {
-      title: "JS date function starts in seconds to current day from what day in 1970",
-      choices: ["January 1", "December 31", "June 1", "April 23"],
-      answer: "January 1"
-    },
-
+        title: "_______is a complete programming language. In contrast, _________ is a coded program that can be introduced to HTML pages. These two languages are not at all inter-dependent and are designed for different intent.",
+        choices: ["Javascript/Script", "BootStrap/Java", "Java/Javascript", "None of the listed"],
+        answer: "Java/Javascript"
+      },
+    {
+        title: "_____ checks only for equality in value, whereas ______is a stricter equality test and returns false if either the value or the type of the two variables are different."
+        choices: ["!=,<=", "==,>=", "==,||", "==,==="],
+        answer: "==,==="
+     },  
+    {
+        title: "What is a valid Timer piece of code ?",
+        choices: ["TimeoutSet", "Interval(delay,function)", "clearSetTimeout(id)", "setInterval(function, delay)"],
+        answer: "setInterval(function, delay)"
+     },
+     {
+        title: " What is === operator?"
+        choices: ["Contains", "Almost Equivalant", "Partially Equal", "Strictly Equal"],
+        answer: "Strictly Equal"
+     },     
   ];
